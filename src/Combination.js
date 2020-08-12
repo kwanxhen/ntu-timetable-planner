f2b2d25fe98a9477feae571a76be8fea3fdcc054
kwanxhen <kwanxhenng@gmail.com>
2020-08-12 23:42:02 +0800

    first commit, incomplete algorithm, unable to dynamically output courses

--- src/Combination.js
+++ src/Combination.js
@@ -1,7 +1,23 @@
 import React, { useState } from "react";
 
 function Combination() {
-  const [isClash, setIsClash] = useState(null);
+  function arraysEqual(a, b) {
+    if (a === b) return true;
+    if (a == null || b == null) return false;
+    if (a.length !== b.length) return false;
+
+    // If you don't care about the order of the elements inside
+    // the array, you should sort both arrays here.
+    // Please note that calling sort on an array will modify that array.
+    // you might want to clone your array first.
+
+    for (var i = 0; i < a.length; ++i) {
+      if (a[i] !== b[i]) return false;
+    }
+    return true;
+  }
+
+  // const [isClash, setIsClash] = useState(null);
 
   //import data from json file
   const myCourses = require("./myCourses2");
@@ -56,32 +72,60 @@ function Combination() {
     }
   }
 
-  //a state variable
-  // let isClash = null;
-
+  let isClash = null;
+  let noClashCombi = [];
+  let passArray = [];
   //using a combination loop function, we compare for clashes
   for (let i = 0; i < cartesianArray.length; i++) {
+    passArray = [];
     for (let j = 0; j < cartesianArray[0].length - 1; j++) {
       for (let k = j + 1; k < cartesianArray[0].length; k++) {
         // isClash = checkClash(cartesianArray[i][j], cartesianArray[i][k]);
-        if (checkClash(cartesianArray[i][j], cartesianArray[i][k])) {
-          setIsClash(true);
-        } else {
-          setIsClash(false);
-        }
+        isClash = checkClash(cartesianArray[i][j], cartesianArray[i][k]);
 
-        console.log(cartesianArray[i][j].slots.day);
-        console.log(cartesianArray[i][j].slots.time);
-        console.log(cartesianArray[i][k].slots.day);
-        console.log(cartesianArray[i][k].slots.time);
-        console.log(isClash);
-        console.log("BEBEBEBE");
+        // console.log(cartesianArray[i][j].slots.day);
+        // console.log(cartesianArray[i][j].slots.time);
+        // console.log(cartesianArray[i][k].slots.day);
+        // console.log(cartesianArray[i][k].slots.time);
+        // console.log(isClash);
+        // console.log("BEBEBEBEBEBEBEBEBEBEBEBEBEBEBBE");
+        if (!isClash) {
+          passArray.push("pass");
+        }
+        //here we know what combination clashes, so we just push this combination that are NOT clashing into an array. Then we do array.map to return components DisplayCourse passing in props which are courseName and slots then inside Display we can access these props with {}.
       }
     }
+    if (arraysEqual(passArray, ["pass", "pass", "pass"])) {
+      console.log("combi is safe");
+      console.log(i);
+      console.log(cartesianArray[i]);
+      noClashCombi.push(cartesianArray[i]);
+    }
   }
+  console.log(noClashCombi);
+
+  const displayClashCombi = noClashCombi.map((combi, index) => {
+    // for (let i=0; i<combi.length; i++) {
+    //   let courseName = combi[i].course;
+    // }
+    let courseName1 = combi[0].course;
+    let 
+    let courseName2 = combi[1].course;
+    let courseName3 = combi[2].course;
+
+    return (
+      <div>
+        <p>{courseName1 + course1Start + course1End}</p>
+        <p>{courseName2}</p>
+        <p>{courseName3}</p>
+        <p>hello</p>
+      </div>
+    );
+  });
 
   return (
     <div>
+      {displayClashCombi}
       <p>Hello world 2</p>
     </div>
   );
--- src/myCourses2.js
+++ src/myCourses2.js
@@ -3,21 +3,21 @@ let myCourses = [
     course: "ee3001",
     slots: [
       {
-        day: "monday",
+        day: "tuesday",
         time: {
           start: 930,
           end: 1500
         }
       },
       {
-        day: "tuesday",
+        day: "wednesday",
         time: {
           start: 1100,
           end: 1600
         }
       },
       {
-        day: "wednesday",
+        day: "thursday",
         time: {
           start: 1330,
           end: 1900
@@ -29,14 +29,14 @@ let myCourses = [
     course: "ee3002",
     slots: [
       {
-        day: "monday",
+        day: "tuesday",
         time: {
           start: 1030,
           end: 1230
         }
       },
       {
-        day: "thursday",
+        day: "wednesday",
         time: {
           start: 1430,
           end: 1630
@@ -57,17 +57,17 @@ let myCourses = [
       {
         day: "wednesday",
         time: {
-          start: 1430,
+          start: 1030,
           end: 1530
         }
       },
-      {
-        day: "thursday",
-        time: {
-          start: 1130,
-          end: 1230
-        }
-      },
+      // {
+      //   day: "thursday",
+      //   time: {
+      //     start: 1130,
+      //     end: 1230
+      //   }
+      // },
     ],
   },
 ];
